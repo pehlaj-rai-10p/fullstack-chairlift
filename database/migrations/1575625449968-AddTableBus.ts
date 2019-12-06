643c5b426d962f1c0ff83d9e16231b77520958f1
Pehlaj Rai <pehlaj.rai@tenpearls.com>
2019-12-06 14:45:06 +0500

    Create migration for Bus table

---
+++ database/migrations/1575625449968-AddTableBus.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class AddTableBus1575625449968 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<any> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<any> {
+    }
+
+}
