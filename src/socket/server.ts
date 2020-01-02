cf08123d5c0651ef362651c7dd8371fc4d8c367a
Pehlaj Rai <pehlaj.rai@tenpearls.com>
2020-01-02 18:13:39 +0500

    Fixes for Socket IO

--- src/socket/server.ts
+++ src/socket/server.ts
@@ -1,6 +1,7 @@
 import { createServer, Server } from 'http';
 import * as Koa from 'koa';
 import * as socketIo from 'socket.io';
+import { Double } from 'typeorm';
 
 export class AppServer {
     public static readonly PORT: number = 4003;
@@ -8,6 +9,8 @@ export class AppServer {
     private server: Server;
     private io: socketIo.Server;
     private port: string | number;
+
+    private randomData = [{ latitude: 52.5162041, longitude: 13.378365 }, { latitude: 52.5162041, longitude: 13.378365 }, { latitude: 52.5162041, longitude: 13.378365 }, { latitude: 52.5159999, longitude: 13.3778999 }, { latitude: 52.5206638, longitude: 13.3861149 }, { latitude: 52.5205999, longitude: 13.3861999 }, { latitude: 52.5162792, longitude: 13.3795345 }, { latitude: 52.5163651, longitude: 13.3808541 }, { latitude: 52.5180817, longitude: 13.3804464 }, { latitude: 52.5189292, longitude: 13.3802962 }];
 
     constructor(app: Koa) {
         this.app = app;
@@ -43,30 +46,51 @@ export class AppServer {
         });
 
         this.io.on('connect', (socket: any) => {
-            console.log('\n\nConnected client on port %s.\n\n', this.port);
-            //this.io.path('/api/v1/bus/info').emit('/', 'Message from server.');
-            // socket.on('event', (_m: any) => {
-            //     console.log('\n\nMessage received from app\n\n');//JSON.stringify(m));
-            //     this.io.sockets.emit('event', { content: 'sending message again' });
+            console.log('\n\nSocket Connected ', socket.port, socket.path);
+
+            // socket.on('open', function () {
+
+            //     socket.on('event', (event: any) => {
+            //         console.log('\n\nEvent received from app is ', event);//JSON.stringify(m));
+            //         socket.emit('event', { content: 'Event response from server.' });
+            //         //this.io.sockets.emit('event', { content: 'Event response from server.' });
+            //     });
+
+            //     //socket.emit('event', { content: 'Event on socket open' });
+            //     //socket.emit('message', { content: 'Message on socket open' });
+            //     socket.on('message', (mesage: any) => {
+
+            //         socket.emit('message', { content: 'Message response from server' });
+            //         console.log('\n\Message received on socket from app is ', mesage);//JSON.stringify(m));
+            //     });
+            //     socket.on('close', function () {
+            //         console.log('\n\Socket close from app3\n\n');//JSON.stringify(m));
+            //     });
             // });
 
-            // socket.on('/', (_m: any) => {
-            //     console.log('\n\nMessage received from app2\n\n');//JSON.stringify(m));
-            //     this.io.sockets.emit('info', { content: 'sending message again2' });
-            // });
-
-            socket.on('event', (_m: any) => {
-                console.log('\n\nMessage received from app3\n\n');//JSON.stringify(m));
-                //this.io.sockets.emit('info', { content: 'sending message again2' });
+            socket.on('message', (mesage: any) => {
+                console.log('\n\nMessage received from app: ', mesage);//JSON.stringify(m));
+                socket.emit('message', { content: 'Message response from server' });
+                //this.io.sockets.emit('message', { content: 'Message response from server' });
             });
 
-            socket.on('event', (_m: any) => {
-                console.log('\n\nMessage received from app2\n\n');//JSON.stringify(m));
-                //this.io.sockets.emit('info', { content: 'sending message again2' });
+            socket.on('event', (event: any) => {
+                console.log('\n\nEvent received from app: ', event);//JSON.stringify(m));
+                const random = Math.round(Math.random() * 10);
+                var data = this.randomData[random];
+                console.log('\n\nData: ', random, data);
+                socket.emit('data', { data: data, content: 'Server message in event response' });
+                //this.io.sockets.emit('event', { content: 'Server message in event response' });
             });
 
             socket.on('disconnect', () => {
                 console.log('Client disconnected');
+            });
+
+            socket.on('transport', (transport: any) => {
+                console.log('\n\nIO Transport from app: ', transport);//JSON.stringify(m));
+                socket.emit('event', { content: 'Transport response from server' });
+                //this.io.sockets.emit('event', { content: 'sending event - transport' });
             });
         });
     }
